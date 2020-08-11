2ae7a4a366c6bce5886f34e39a04f487b9ebb601
This Guy Code <hws.thisguycode@gmail.com>
2020-08-12 00:08:21 +0800

    Create a Eureka Service

---
+++ README.md
@@ -0,0 +1,1 @@
+Following the blog post - https://developer.okta.com/blog/2017/06/15/build-microservices-architecture-spring-boot
---
+++ eureka-service/src/main/java/my/itchyfingers/microbrews/eureka/EurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package my.itchyfingers.microbrews.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
